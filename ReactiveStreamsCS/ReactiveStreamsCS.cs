0123af20309529bff2c933f5bb2ba73ac5bf05cd
Dávid Karnok <akarnokd@gmail.com>
2015-12-17 20:51:18 +0100

    Added base interfaces Publisher, Single, Completable

--- ReactiveStreamsCS/ReactiveStreamsCS.cs
+++ ReactiveStreamsCS/ReactiveStreamsCS.cs
@@ -8,6 +8,62 @@ namespace ReactiveStreamsCS
 {
     public interface IPublisher<out T>
     {
+        void Subscribe(ISubscriber<T> s);
+    }
 
+    public interface ISubscriber<in T>
+    {
+        void OnSubscribe(ISubscription s);
+
+        void OnNext(T t);
+
+        void OnError(Exception e);
+
+        void OnComplete();
+    }
+
+    public interface ISubscription
+    {
+        void Request(long n);
+
+        void Cancel();
+    }
+
+    public interface IProcessor<in T, out R> : IPublisher<R>, ISubscriber<T>
+    {
+
+    }
+
+    public interface ISingle<out T>
+    {
+        void Subscribe(ISingleSubscriber<T> s);
+    }
+
+    public interface ISingleSubscriber<in T>
+    {
+        void OnSubscribe(IDisposable d);
+
+        void OnSuccess(T t);
+
+        void OnError(Exception e);
+    }
+
+    public interface ISingleProcessor<in T, out R> : ISingle<R>, ISingleSubscriber<T>
+    {
+
+    }
+
+    public interface ICompletable
+    {
+        void Subscribe(ICompletableSubscriber s);
+    }
+
+    public interface ICompletableSubscriber
+    {
+        void OnSubscribe(IDisposable d);
+
+        void OnError(Exception e);
+
+        void OnComplete();
     }
 }
